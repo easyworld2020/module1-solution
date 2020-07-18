f93541c04469a062f4c2b91968c957c5064e40fb
Your Name <you@example.com>
2020-07-17 17:48:51 -0700

    module 1 - solution

---
+++ app.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+  
+  angular.module('LunchCheckApp', [])
+  .controller('LunchCheckController', LunchCheckController);
+  
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.lunchList = "";
+    $scope.message = "";
+
+
+    $scope.displayMessage = function () {
+      var lunchList = $scope.lunchList.trim();
+      if (lunchList) {
+        var lunchItems = lunchList.split(",");
+        if ((lunchItems.length >3 && lunchItems[3]) || lunchItems.length >4 ) {
+          $scope.message = "Too much!";
+        } else {
+          $scope.message = "Enjoy!";
+        }
+      } else {
+        $scope.message ='';
+      }
+    };
+  }
+  })();
+  
+  
